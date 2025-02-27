316c1da50bcef12ea09625b1c13ab574d2c462d4
Jawad Ali <57399800+jawad-ali786@users.noreply.github.com>
2025-02-27 15:31:09 +0500

    "Changes for Vercel "

--- src/app.js
+++ src/app.js
@@ -17,7 +17,8 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
-app.use('/api/upload', express.static('uploads'));
+app.use('/api/upload', express.static('public/uploads'));
+
 
 
 
--- src/utils/function.js
+++ src/utils/function.js
@@ -1,6 +1,8 @@
-const getImagePath = (image) => {
-    // Joins the upload directory path with the image file name to create a full path
-    return path.join("uploads", image)
-}
-export { getImagePath }
+import path from 'path';
 
+const getImagePath = (image) => {
+    // Use an absolute path that resolves correctly in both local and Vercel environments
+    return path.join(__dirname, '..', 'public', 'uploads', image); // Adjust based on your structure
+}
+
+export { getImagePath }
