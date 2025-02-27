ce86b5fe3ce5df53036211a66d48d184336965f2
SYED-MHAMIL <syedmahamil56@gmail.com>
2025-02-27 02:41:18 -0800

    'setapp'

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
-app.use('/api/uploads', express.static('uploads'));
+app.use('/api/upload', express.static('uploads'));
 
 
 
